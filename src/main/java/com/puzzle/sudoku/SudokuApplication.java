ef19fb8bb0145bc4f0d02dc2bc64d0a03ba2a45c
abhi870 <abhishekpaithane@gmail.com>
2021-07-29 08:45:06 +0530

    validator updated

--- src/main/java/com/puzzle/sudoku/SudokuApplication.java
+++ src/main/java/com/puzzle/sudoku/SudokuApplication.java
@@ -4,6 +4,7 @@ import com.puzzle.sudoku.model.BoardColumnsValidator;
 import com.puzzle.sudoku.model.BoardRowsValidator;
 import com.puzzle.sudoku.model.BoardSubSquareBoxValidator;
 import com.puzzle.sudoku.model.SudokuBoard;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +20,14 @@ public class SudokuApplication {
 		SpringApplication.run(SudokuApplication.class, args);
 	}
 
+
+	@Autowired
+	private BoardRowsValidator rowsValidator;
+	@Autowired
+	private BoardColumnsValidator columnsValidator;
+	@Autowired
+	private BoardSubSquareBoxValidator subSquareBoxValidator;
+
 	@Bean
 	public CommandLineRunner demo(){
 		return new CommandLineRunner() {
@@ -33,18 +42,16 @@ public class SudokuApplication {
 				row3.add(3);row3.add(4);row3.add(1);row3.add(2);
 				List<Integer> row4 = new ArrayList<>();
 				row4.add(4);row4.add(1);row4.add(2);row4.add(3);
-//				List<Integer> row1 = new ArrayList<>();
-//				List<Integer> row2 = new ArrayList<>();
-//				row1.add(1); row1.add(2);
-//				row2.add(2);row2.add(1);
+
+
 				currentboard.add(row1);
 				currentboard.add(row2);
 				currentboard.add(row3);
 				currentboard.add(row4);
 				SudokuBoard board = new SudokuBoard(currentboard, new ArrayList<>());
-				board.getValidators().add(new BoardRowsValidator());
-				board.getValidators().add(new BoardColumnsValidator());
-				board.getValidators().add(new BoardSubSquareBoxValidator());
+				board.getValidators().add(rowsValidator);
+				board.getValidators().add(columnsValidator);
+				board.getValidators().add(subSquareBoxValidator);
 				System.out.println(board.validateBoardState());
 			}
 		};
